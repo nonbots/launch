f6b41cda5f0d4086a9afaa198cd50b25257bf919
Nhan Bui <nhantdbui@gmail.com>
2022-08-16 17:52:34 +0000

    calculator ready for review

--- core/JS101/lesson2/calculator.js
+++ core/JS101/lesson2/calculator.js
@@ -1,41 +1,49 @@
 // VARIABLES AND CALLS
-
 const rl = require('readline-sync');
 
-prompt('Welcome to Calulator');
+const messages = require('./calculator_messages.json'); 
 
-let num1 = getNumber('first');
+prompt(messages.en.welcome + '\n' + messages.en.pickLanguage + '\n');
+prompt(messages.fr.welcome + '\n' + messages.fr.pickLanguage);
 
-let num2 = getNumber('second');
+let language = rl.question(); 
+language = validateLanguage(language, requestValidInput); 
 
-let operation = getOperation();
+let newCalInput;
 
-let output = getCalculation(num1, num2, operation);
-
-console.log(`Answer: ${num1} ${operation} ${num2} = ${output}`);
-
+/*
+runs calculator and getValidInput once before evaluting condition
+loops through statements if user choose to do a new calculation
+*/
+do {
+  calculator(); 
+  newCalInput = getValidInput(messages[language].newCal, validateNewCal)
+} while (newCalInput === 'y'); 
 
 // FUNCTIONS
-
-// prompts a user input and returns a valid input, a number
-function getNumber(inputValue) {
-  prompt('Enter the ' + inputValue + ' number');
-  let num = rl.question();
-  return validateNum(num, requestValidInput);
+/*
+prompts the user for an input
+returns a valid input based on the callback
+*/
+function getValidInput(message, cb) {
+  prompt(message); 
+  let input = rl.question(); 
+  return cb(input, requestValidInput); 
 }
 
-// prompts a user input and return a valid opertion
-function getOperation() {
-  prompt(`Enter the operation: 
-  Enter '*' for multiply 
-  Enter '+' for plus 
-  Enter '-' for minus 
-  Enter '/' for divide`);
-  let operation = rl.question();
-  return validateOperation(operation, requestValidInput);
+/*
+retieves valid numbers and operation to be passed to getCalculation
+log the result in the console
+*/
+function calculator() {
+  let num1 = getValidInput(messages[language].firstNumber, validateNum);
+  let num2 = getValidInput(messages[language].secondNumber, validateNum);
+  let operation = getValidInput(messages[language].operation, validateOperation);
+  let output = getCalculation(num1, num2, operation);
+  console.log(`Answer: ${num1} ${operation} ${num2} = ${output}`);
 }
 
-// returns the result, a number, after numbers go through operation
+// returns the result, a number, based on the operation chosen by the user
 function getCalculation(input1, input2, operation) {
   let output;
   switch (operation) {
@@ -56,11 +64,25 @@ function getCalculation(input1, input2, operation) {
 }
 
 // HELPER FUNCTIONS
+//prompts user user input until 'en' or 'fr' is returned
+function validateLanguage(input, cb) {
+  while (input !== 'en' && input !== 'fr') {
+    input = cb(messages.en.invalidLanguage + '\n' + messages.fr.invalidLanguage); 
+  }
+  return input;
+}
+// prompts user input until 'y' or 'n' ir returned
+function validateNewCal(input, cb) {
+  while (input !== 'n' && input !== 'y') {
+    input = cb(messages[language].invalidNewCal);
+  }
+  return input; 
+}
 
 // prompts user input until a number is returned
 function validateNum(num, cb) {
   while (isNaN(num) || num.trim() === "") {
-    num = cb('number');
+    num = cb(messages[language].invalidNumber);
   }
   return Number(num);
 }
@@ -71,18 +93,18 @@ function validateOperation(operation, cb) {
         operation !== '/' &&
         operation !== '-' &&
         operation !== '+') {
-    operation = cb(operation, 'operation');
+    operation = cb(messages.invalidOperation);
   }
   return operation;
 }
 
-// prompts the user for input when invalid and returns user input
-function requestValidInput(value) {
-  prompt('Enter a valid ' + value);
+// prompts the user for an input and returns user input
+function requestValidInput(message) {
+  prompt(message);
   return rl.question();
 }
 
-// logs the message to the user
+//logs the message to the user with a marker
 function prompt(message) {
   console.log(`=> ${message}`);
 }
